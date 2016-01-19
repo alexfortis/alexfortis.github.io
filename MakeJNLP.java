2e5ec51226c0d05b466aada6e35bc61bec21e8d8
Alexander Strong <alex.j.strong@gmail.com>
2016-01-19 12:57:56 -0500

    remade JNLP

--- MakeJNLP.java
+++ MakeJNLP.java
@@ -18,22 +18,22 @@ public class MakeJNLP {
 	    File folder = new File(f.getPath()+"/Excel_IPs/libs");
 	    PrintStream jnlpStream = new PrintStream(jnlp), scriptStream = new PrintStream(script);
 	    jnlpStream.println("<jnlp spec=\"1.0+\" codebase=\"https://alexfortis.github.io/Excel_IPs\" href=\"" + jnlp.getName() + "\"/>");
-	    jnlpStream.println("\t<information>");
-	    jnlpStream.println("\t\t<title>Excel IPs</title>");
-	    jnlpStream.println("\t\t<vendor>Alex Strong</vendor>");
-	    jnlpStream.println("\t\t<description>Edits Excel spreadsheets that determine who should be banned from NYSPI's network for lack of OCS</description>");
-	    jnlpStream.println("\t\t<offline-allowed/>");
-	    jnlpStream.println("\t</information>");
+	    jnlpStream.println("  <information>");
+	    jnlpStream.println("    <title>Excel IPs</title>");
+	    jnlpStream.println("    <vendor>Alex Strong</vendor>");
+	    jnlpStream.println("    <description>Edits Excel spreadsheets that determine who should be banned from NYSPI's network for lack of OCS</description>");
+	    jnlpStream.println("    <offline-allowed/>");
+	    jnlpStream.println("  </information>");
 	    JarFilter jf = new JarFilter();
-	    jnlpStream.println("\t<resources>");
-	    jnlpStream.println("\t\t<j2se version=\"1.8+\" href=\"java.oracle.com/products/autodl/j2se\" java-vm-args=\"-Djava.net.preferIPv4Stack=true\"/>");
+	    jnlpStream.println("  <resources>");
+	    jnlpStream.println("    <j2se version=\"1.8+\" href=\"java.oracle.com/products/autodl/j2se\" java-vm-args=\"-Djava.net.preferIPv4Stack=true\"/>");
 	    File[] jars = folder.listFiles(jf);
 	    String[] names = new String[jars.length];
 	    //System.out.println("Processing files...");
 	    for(int i = 0; i < jars.length; i++) {
 		names[i] = folder.getName() + "/" + jars[i].getName();
 		//System.out.println("Now processing " + names[i]);
-		jnlpStream.println("\t\t<jar href=\"" + names[i] + "\"/>");
+		jnlpStream.println("    <jar href=\"" + names[i] + "\"/>");
 		String addManifest = "jar ufm " + names[i] + " ../manifest.txt";
 		String signJar = "jarsigner " + names[i] + " alexstrong";
 		scriptStream.println("echo \"" + addManifest + "\"");
@@ -41,9 +41,9 @@ public class MakeJNLP {
 		scriptStream.println("echo \"" + signJar + "\"");
 		scriptStream.println(signJar);
 	    }
-	    jnlpStream.println("\t</resources>");
-	    jnlpStream.println("\t<security>\n\t\t<all-permissions/>\n\t</security>");
-	    jnlpStream.println("\t<application-desc main-class=\"com.alexstrong.excel_ips.ExcelDriver\"/>");
+	    jnlpStream.println("  </resources>");
+	    jnlpStream.println("  <security>\n    <all-permissions/>\n  </security>");
+	    jnlpStream.println("  <application-desc main-class=\"com.alexstrong.excel_ips.ExcelDriver\"/>");
 	    jnlpStream.println("</jnlp>");
 	    //System.out.println("Done.");
 	}
