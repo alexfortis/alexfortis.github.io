18be6799157783dad602c1219b92d179c85d03ce
Alexander Strong <alex.j.strong@gmail.com>
2016-01-19 22:55:01 -0500

    fixed commit.sh

--- commit.sh
+++ commit.sh
@@ -2,7 +2,7 @@ echo "git add ."
 git add .
 echo "Input commit message:"
 read msg
-echo "git commit -m $msg"
-git commit -m "\"$msg\""
+echo "git commit -m \"$msg\""
+git commit -m "$msg"
 echo "git push origin master"
 git push origin master
